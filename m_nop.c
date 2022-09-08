0fb0cd81f18eb0bc7fdfa51537846b1e51057d93
Arnold Mdluli <arnoldmdluli00@gmail.com>
2022-09-08 07:01:42 -0700

    Task 5

---
+++ m_nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+#include "lists.h"
+
+/**
+ * m_nop - handles the nop instruction
+ * @stack: double pointer to the stack to push to
+ * @line_number: number of the line in the file
+ */
+void m_nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
