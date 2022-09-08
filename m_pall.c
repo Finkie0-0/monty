779f2152b9aa40c498e9662962f40d2473ecf43d
Finkie0-0 <104798727+Finkie0-0@users.noreply.github.com>
2022-09-08 23:44:31 +0200

    Update m_pall.c

--- m_pall.c
+++ m_pall.c
@@ -8,7 +8,18 @@
  */
 void m_pall(stack_t **stack, unsigned int line_number)
 {
-	(void)line_number;
-	if (*stack)
-		print_dlistint(*stack);
+	stack_t *head;
+
+	(void)(line_number);
+
+	head = *stack;
+	while (head != NULL)
+	{
+		printf("%d\n", head->n);
+		head = head->next;
+		if (head == *stack)
+		{
+			return;
+		}
+	}
 }
